61c42612edddc56982596851dab240f361236816
Benson <D000019611@cgu.edu.tw>
2025-10-17 13:31:56 +0800

    feat: 1.update 主區塊間距

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,31 +1,33 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark height="64">
-      <v-app-bar-title>
-        <v-icon left> mdi-clock </v-icon>
-        陳俊生小程式工具平台
-      </v-app-bar-title>
+    <v-app-bar color="primary" height="64">
+      <v-container fluid class="d-flex" style="height: 100%">
+        <v-app-bar-title>
+          <v-icon left> mdi-clock </v-icon>
+          陳俊生小程式工具平台
+        </v-app-bar-title>
 
-      <v-spacer />
+        <v-spacer />
 
-      <v-btn to="/" text class="mr-2">
-        <v-icon left> mdi-home </v-icon>
-        首頁
-      </v-btn>
+        <v-btn to="/" text class="mr-2">
+          <v-icon left> mdi-home </v-icon>
+          首頁
+        </v-btn>
 
-      <v-btn to="/time-calculator" text class="mr-2">
-        <v-icon left> mdi-calculator </v-icon>
-        工時計算器
-      </v-btn>
+        <v-btn to="/time-calculator" text class="mr-2">
+          <v-icon left> mdi-calculator </v-icon>
+          工時計算器
+        </v-btn>
 
-      <v-btn to="/savings-calculator" text>
-        <v-icon left> mdi-piggy-bank </v-icon>
-        存錢計算器
-      </v-btn>
+        <v-btn to="/savings-calculator" text>
+          <v-icon left> mdi-piggy-bank </v-icon>
+          存錢計算器
+        </v-btn>
+      </v-container>
     </v-app-bar>
 
     <v-main>
-      <v-container fluid>
+      <v-container>
         <slot />
       </v-container>
     </v-main>
